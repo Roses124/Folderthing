a1563240173903ddd8a09387f9b62fb1e94ed15c
Gabby Yurchak <yurchak03@gmail.com>
2019-07-15 14:31:03 -0400

    The og guessing game.

---
+++ Numbergame.py
@@ -0,0 +1,21 @@
+#imports the ability to get a random number (we will learn more about this later!)
+from random import *
+
+#Generates a random integer.
+aRandomNumber = randint(1, 20)
+tries = 0
+while tries < 3:
+    guess = input("Guess a number between 1 and 20 (inclusive): ")
+    if not guess.isnumeric(): # checks if a string is only digits 0 to 9
+        print("That's not a positive whole number, try again!")
+    else:
+        guess = int(guess) # converts a string to an integer
+    if guess == aRandomNumber:
+        print("Good job!")
+        break
+    elif guess!= aRandomNumber:
+        tries+=1
+        if guess < aRandomNumber:
+            print("Pick a higher number!")
+        elif guess > aRandomNumber:
+            print("Pick a lower number!")
